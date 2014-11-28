471040ee169604182fa2f32a2da64d12ad2c981f
Miguel A. Moreno <miguel@tyba.com>
2014-11-28 17:43:01 +0100

    Initial Commit

---
+++ TouchKracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchKracker
+//
+//  Created by Miguel Angel Moreno Armenteros on 28/11/14.
+//  Copyright (c) 2014 Miguel Angel Moreno Armenteros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
